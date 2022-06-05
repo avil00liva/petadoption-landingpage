1c89d7092e44f0a4cb75d5fdc7a08ae3bf38db6d
avil00liva <agustinoli54@gmail.com>
2022-06-05 10:38:43 -0300

    Cambio en rama de prueba

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
   return (
     <header>
         <h1>Una mano por cuatro patas</h1>
+        <h2>Hola esto es una prueba</h2>
         <p>Buscamos combatir el abandono y maltrato animal desde el respeto y el amor por los animales.</p>
         <FindButton />
     </header>
